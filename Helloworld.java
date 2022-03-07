2f4e8dc820bd352d0e9eef5ad89fc3b077f6241e
pramuditaa <92425838+pramuditaa@users.noreply.github.com>
2022-03-07 22:00:54 +0700

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,10 @@
+public class Helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("Halo, nama saya adalah Andana Daniswara");
+		System.out.println("Saya adalah mahasiswa Teknik Informatika Universitas Dian Nuswantoro");
+		System.out.println("Saya sedang menulis program java");
+
+	}
+
+}
